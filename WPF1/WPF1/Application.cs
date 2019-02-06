04f0a3f7c8f0032b35227088f73ad0057efc1aec
gupt7868 <gupt7868@PCB-LAB360-35>
2019-02-06 11:03:24 -0600

    Participation

---
+++ WPF1/WPF1/Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class Application
+    {
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+   
+    }
+
+    public Application()
+
+    {
+
+    }
+}
